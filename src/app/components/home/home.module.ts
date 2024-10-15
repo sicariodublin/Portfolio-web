d039a13adeb1e3be288ed94158a48f49225fec5d
sicariodublin <fabioast47@hotmail.com>
2024-10-15 11:46:12 +0100

    feat: updated

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -4,17 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { MainAnimationComponent } from 'src/app/shared/main-animation/main-animation.component';
 import { MainSectionComponent } from 'src/app/shared/main-section/main-section.component';
+import { AboutMeComponent } from '../../shared/about-me/about-me.component';
 import { BntPrimaryComponent } from '../../shared/bnt-primary/bnt-primary.component';
+import { ContactComponent } from '../../shared/contact/contact.component'; // Import the Contact component
 import { HeaderComponent } from '../../shared/header/header.component';
+import { ProjectsComponent } from '../../shared/projects/projects.component';
 import { HomeComponent } from './home.component';
-
 @NgModule({
   declarations: [
     HomeComponent,
     HeaderComponent,
     BntPrimaryComponent,
     MainSectionComponent,
-    MainAnimationComponent
+    MainAnimationComponent,
+    AboutMeComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
