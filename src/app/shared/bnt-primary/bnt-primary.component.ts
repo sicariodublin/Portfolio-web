74b6ed9f4398bf1d10b492a7cfd0ff832b00d12f
sicariodublin <fabioast47@hotmail.com>
2024-10-15 11:46:59 +0100

    feat: changed styles

--- src/app/shared/bnt-primary/bnt-primary.component.ts
+++ src/app/shared/bnt-primary/bnt-primary.component.ts
@@ -6,5 +6,11 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./bnt-primary.component.scss']
 })
 export class BntPrimaryComponent {
-  @Input() text: string = '';
+  @Input() text: string = 'Download Resume';
+  @Input() download: boolean = false; // New input to differentiate button types
+  @Input() filePath: string = 'assets/Resume.pdf'; // Path to the resume file in assets folder
+  @Input() fileName: string = 'Resume.pdf'; // Name for the downloaded file
+  @Input() modalTrigger: boolean = false; // Add modal trigger input for "Contact Me"
+  
+
 }
